5a25a7ec93c96a98c75d1ef8050aeb5813055c9c
prasannarani-bit <prasannaranipatnana2003@gmail.com>
2026-04-09 13:33:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config['SECRET_KEY'] = 'super_secret_agentic_key'
 
 @app.route("/")
 def home():
-    return render_template("login.html")
+    return "App working ✅"
 
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
